916ad1688763f2b6a5b87aca9fd7f12d3f56a0e8
Alexis Reigel <mail@koffeinfrei.org>
2011-02-18 15:55:42 +0100

    * fixed versionize script * updated version to 0.0.2

--- versionize.rb
+++ versionize.rb
@@ -12,13 +12,20 @@ PackageCodeGuid = UUIDTools::UUID.random_create.to_s.upcase
 
 # update version and guids for setup.vdproj
 ['version.txt', 
- 'Koffeinfrei.Zueribad/Properties/AssemblyInfo.cs', 
- 'Koffeinfrei.Zueribad.Setup/Koffeinfrei.Zueribad.Setup.vdproj'].each do |filename|
+ 'Koffeinfrei.MinusShare/Properties/AssemblyInfo.cs'].each do |filename|
     puts "> updating '#{filename}'..."
     contents = File.read(filename)
     File.open(filename, 'w') do |file|
         file.puts contents
                     .gsub(/\d\.\d\.\d\.\d/, "#{version}")
+    end
+end
+
+['Koffeinfrei.MinusShare.Setup/Koffeinfrei.MinusShare.Setup.vdproj'].each do |filename|
+    puts "> updating '#{filename}'..."
+    contents = File.read(filename)
+    File.open(filename, 'w') do |file|
+        file.puts contents
                     .gsub(/("ProductVersion" = "8:)\d\.\d\.\d"/, "\\1#{version[/\d\.\d\.\d/]}\"")
                     .gsub(/("ProductCode" = "8:){[^}]+}"/, "\\1{#{ProductCodeGuid}}\"")
                     .gsub(/("PackageCode" = "8:){[^}]+}"/, "\\1{#{PackageCodeGuid}}\"")
